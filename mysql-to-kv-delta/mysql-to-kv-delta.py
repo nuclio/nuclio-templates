2ce78a47880dfa58896ad61659a24b8234b6b3a6
anantg <anant1985@gmail.com>
2019-04-29 20:56:22 +0800

    formatting changes

--- mysql-to-kv-delta/mysql-to-kv-delta.py
+++ mysql-to-kv-delta/mysql-to-kv-delta.py
@@ -33,30 +33,35 @@ DELTA_INTERVAL_MINUTE = os.environ['DELTA_INTERVAL_MINUTE']
 MODIFIED_DATETIME_COL = os.environ['MODIFIED_DATETIME_COL']
 CREATED_DATETIME_COL = os.environ['CREATED_DATETIME_COL']
 
+# Iguazio platform variables
+IGZ_V3F=os.environ['IGZ_V3F']
+IGZ_USER=os.environ['IGZ_USER']
+IGZ_PWD=os.environ['IGZ_PWD']
+CONTAINER=os.environ['CONTAINER']
+
 
 def handler(context, event):
     datetime_query = str(datetime.datetime.now() - datetime.timedelta(seconds=int(DELTA_INTERVAL_MINUTE) * 60))
     sql_query_diff = f"{SQL_QUERY} where ({CREATED_DATETIME_COL}>='{str(datetime_query)}' AND\
      {MODIFIED_DATETIME_COL} IS NULL) OR ({MODIFIED_DATETIME_COL}>='{str(datetime_query)}')"
 
-    # sql_query_diff = SQL_QUERY + " where (" + CREATED_DATETIME_COL + ">='" + str(datetime_query) +
-    # "' AND " + MODIFIED_DATETIME_COL + " IS NULL) OR (" + MODIFIED_DATETIME_COL + ">='" + str(datetime_query) + "')"
-
+   
     # for debugging the generated sql query
-    context.logger.debug("Generated sql query", sql_query_diff)
+    context.logger.debug('Generated sql query', sql_query_diff)
     df = pd.read_sql(sql_query_diff, context.dbconn.connection())
 
     # for debugging no of rows
-    context.logger.debug("No of delta rows :: {} ".format(df.shape[0]))
+    context.logger.debug('Read SQL database table into a DataFrame ',no_delta_rows=df.shape[0])
     context.client.write(backend='kv', table=os.getenv('TABLE'), dfs=df)
 
 
 def init_context(context):
+    
     # Init v3io-frames connection and set it as a context attribute
-    client = v3f.Client(address=os.getenv('IGZ_V3F'),
-                        username=os.getenv('IGZ_USER'),
-                        password=os.getenv('IGZ_PWD'),
-                        container=os.getenv('CONTAINER'))
+    client = v3f.Client(address={IGZ_V3F},
+                        user={IGZ_USER},
+                        password={IGZ_PWD},
+                        container={CONTAINER})
     setattr(context, 'client', client)
 
     connection_string = f"mysql://{SQL_USER}:{SQL_PWD}@{SQL_HOST}:{SQL_PORT}/{SQL_DB_NAME}"
