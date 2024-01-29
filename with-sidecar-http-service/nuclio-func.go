570a1e3b723bd6c09757dc1029bedef6956c310b
rokatyy <ekaterina_molchanova@mckinsey.com>
2024-01-29 13:50:43 +0000

    Tomer's comments

--- with-sidecar-http-service/nuclio-func.go
+++ with-sidecar-http-service/nuclio-func.go
@@ -2,23 +2,31 @@ package main
 
 import (
 	"fmt"
-	nuclio "github.com/nuclio/nuclio-sdk-go"
-	"github.com/valyala/fasthttp"
 	"net/url"
 	"os"
+
+	nuclio "github.com/nuclio/nuclio-sdk-go"
+	"github.com/valyala/fasthttp"
 )
 
 func Handler(context *nuclio.Context, event nuclio.Event) (interface{}, error) {
-	context.Logger.Info("Got request, sending it to sidecar container")
-	req := fasthttp.AcquireRequest()
-	req.SetBody(event.GetBody())
 	sidecarHost := os.Getenv("SIDECAR_HOST")
 	sidecarPort := os.Getenv("SIDECAR_PORT")
+	// url for request forwarding
 	sidecarUrl, _ := url.JoinPath(fmt.Sprintf("%s:%s", sidecarHost, sidecarPort), event.GetPath())
-	context.Logger.Info(sidecarUrl)
-	req.SetRequestURI(sidecarUrl)
+
+	// create new request to forward
+	req := fasthttp.AcquireRequest()
 	resp := fasthttp.AcquireResponse()
+
+	// set body and request uri
+	req.SetBody(event.GetBody())
+	req.SetRequestURI(sidecarUrl)
+
+	// sending request to sidecar
+	context.Logger.InfoWith("Forwarding request to sidecar", "sidecarUrl", sidecarUrl)
 	err := fasthttp.Do(req, resp)
+
 	return nuclio.Response{
 		StatusCode:  resp.StatusCode(),
 		ContentType: "application/text",
