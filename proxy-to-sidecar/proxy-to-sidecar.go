5f1763396369966c7500c2755f826ac757c329a5
TomerShor <tomer.shor@gmail.com>
2024-03-07 18:17:53 +0200

    [Sidcar Proxy] Enrich sidecar host and populate request headers

--- proxy-to-sidecar/proxy-to-sidecar.go
+++ proxy-to-sidecar/proxy-to-sidecar.go
@@ -8,6 +8,7 @@ import (
 	"net/http/httputil"
 	"net/url"
 	"os"
+	"strings"
 
 	nuclio "github.com/nuclio/nuclio-sdk-go"
 )
@@ -21,6 +22,12 @@ func Handler(context *nuclio.Context, event nuclio.Event) (interface{}, error) {
 	if err != nil {
 		return nil, err
 	}
+
+	// set headers
+	for k, v := range event.GetHeaders() {
+		httpRequest.Header[k] = []string{v.(string)}
+	}
+
 	recorder := httptest.NewRecorder()
 	reverseProxy.ServeHTTP(recorder, httpRequest)
 
@@ -32,6 +39,10 @@ func Handler(context *nuclio.Context, event nuclio.Event) (interface{}, error) {
 	for key, value := range response.Header {
 		headers[key] = value[0]
 	}
+
+	// let the processor calculate the content length
+	delete(headers, "Content-Length")
+
 	return nuclio.Response{
 		StatusCode:  response.StatusCode,
 		Body:        recorder.Body.Bytes(),
@@ -42,6 +53,14 @@ func Handler(context *nuclio.Context, event nuclio.Event) (interface{}, error) {
 func InitContext(context *nuclio.Context) error {
 	sidecarHost := os.Getenv("SIDECAR_HOST")
 	sidecarPort := os.Getenv("SIDECAR_PORT")
+
+	// enrich sidecar host
+	if sidecarHost == "" {
+		sidecarHost = "http://localhost"
+	} else if !strings.Contains(sidecarHost, "://") {
+		sidecarHost = fmt.Sprintf("http://%s", sidecarHost)
+	}
+
 	// url for request forwarding
 	sidecarUrl := fmt.Sprintf("%s:%s", sidecarHost, sidecarPort)
 	parsedURL, err := url.Parse(sidecarUrl)
